d9d8eeb9b5e86d3c486987aae270e15f2e5a8abe
Vova Shkil <vshkil@tucows.com>
2019-03-22 11:19:38 -0400

    Fibonacci (MAX Integer)

--- src/algorithms/recursion/Fibonacci.java
+++ src/algorithms/recursion/Fibonacci.java
@@ -15,10 +15,18 @@ public class Fibonacci {
 
     public static void main(String[] args) {
 
-        System.out.printf("fib(%d) = %d\n", 9, fib(9, 0, 1));
-        System.out.printf("fib(%d) = %d\n", 10, fib(10, 0, 1));
+        for (int i = 0; i < 11; i++)
+            System.out.printf("fib(%d) = %d\n", i, fib(i, 0, 1));
+
         System.out.printf("fib(%d) = %d\n", 20, fib(20, 0, 1));
         System.out.printf("fib(%d) = %d\n", 30, fib(30, 0, 1));
+
+        for (int i = 40; i < 47; i++)
+            System.out.printf("fib(%d) = %d\n", i, fib(i, 0, 1));
+
+        System.out.printf("MAX Integer = %d\n", Integer.MAX_VALUE);
+        System.out.printf("fib(%d) = %d\n", 47, fib(47, 0, 1));
+
 
     }
 
